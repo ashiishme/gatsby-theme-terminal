9e3726aad197b7e238777334105717a084636cd9
ashiishme <iamashiishy@gmail.com>
2022-01-04 21:21:45 +0100

    reafactor: add post css plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,12 @@
 module.exports = ({ contentPath = 'content', basePath = '/' }) => ({
   plugins: [
+    `gatsby-plugin-postcss`,
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: contentPath,
-        name: 'blog',
       },
     },
     'gatsby-plugin-mdx',
-    'gatsby-plugin-theme-ui',
   ],
 })
