32ff8409dc00f8e8154d3d3ffd9133f255236ca6
ashiishme <iamashiishy@gmail.com>
2022-01-04 21:24:53 +0100

    refactor: add tailwind support with post css

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    '../node_modules/gatsby-theme-terminal/src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
