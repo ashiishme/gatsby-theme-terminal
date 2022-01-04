9bc63f2977ede2bcdc7fe415193ded9703329ab3
ashiishme <iamashiishy@gmail.com>
2022-01-04 21:23:53 +0100

    refactor: add index page in theme

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+      <p>
+        Hi, I am a <strong>Terminal Theme</strong>
+      </p>
+    </>
+  )
+}
+
+export default Home
