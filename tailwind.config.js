2bc273ecc073cadfae77c2f7701190cc270bb597
ashiishme <iamashiishy@gmail.com>
2022-01-05 23:34:57 +0100

    refactor: extend colors & font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     '../node_modules/gatsby-theme-terminal/src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mirage: '#20222E',
+        pacificBlue: '#00ACC1',
+        cinnabar: '#E53935',
+        fruitSalad: '#43A047',
+        gorse: '#FDD835',
+      },
+      fontFamily: {
+        sans: ['Fira Code'],
+      },
+    },
   },
   plugins: [],
 }
