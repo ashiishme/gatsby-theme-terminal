34bfb0a6994483dd9990c10d7ec5a8f88461d8df
ashiishme <iamashiishy@gmail.com>
2022-01-12 00:19:13 +0100

    refactor: update command names

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import Terminal from '../components/terminal'
+import useCommand from '../hooks/useCommand'
 
 const Home = () => {
+  const { executeCommand } = useCommand()
+
+  executeCommand('whoami')
+  executeCommand('testcommand')
+
   return (
     <>
       <Terminal />
