49ad83d9f319508a261e833c98dfd9e9dbc7c427
ashiishme <iamashiishy@gmail.com>
2022-01-13 00:40:09 +0100

    refactor: clean up index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,7 @@
 import React from 'react'
 import Terminal from '../components/terminal'
-import useCommand from '../hooks/useCommand'
 
 const Home = () => {
-  const { executeCommand } = useCommand()
-
-  executeCommand('whoami')
-  executeCommand('testcommand')
-
   return (
     <>
       <Terminal />
