d3aac6c1d5902c6fb1bcef02c3069cd82da56fa4
ashiishme <iamashiishy@gmail.com>
2022-01-20 23:20:41 +0100

    feat: add history hook with context types

---
+++ src/hooks/useHistory.tsx
@@ -0,0 +1,24 @@
+import React, { useContext } from 'react'
+import { HistoryContext } from '../context/HistoryContext'
+
+export type HistoryProps = {
+  type: 'command' | 'output' | 'error'
+  data: string
+}
+
+export type HistoryContextProps = {
+  history: HistoryProps[]
+  addHistory: (item: HistoryProps) => void
+}
+
+const useHistory = () => {
+  const { history, addHistory } =
+    useContext<HistoryContextProps>(HistoryContext)
+
+  return {
+    history,
+    addHistory,
+  }
+}
+
+export default useHistory
