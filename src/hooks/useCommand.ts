7eb1f22ba083139f89cf6d3299f5a5ed1b8cbe85
ashiishme <iamashiishy@gmail.com>
2022-01-20 23:18:59 +0100

    refactor: set history data when a command is executed

--- src/hooks/useCommand.ts
+++ src/hooks/useCommand.ts
@@ -1,13 +1,21 @@
+import { useCallback } from 'react'
 import { CommandFactory } from '../domain/commandFactory'
 
+import useHistory from './useHistory'
+
 const useCommand = () => {
+  const { addHistory } = useHistory()
   const commandFactory = new CommandFactory()
   commandFactory.init()
 
-  const executeCommand = (name: string) => {
+  const executeCommand = useCallback((name: string) => {
     const command = commandFactory.getCommand(name)
     command.execute()
-  }
+    addHistory({
+      type: 'command',
+      data: name,
+    })
+  }, [])
 
   return {
     executeCommand,
