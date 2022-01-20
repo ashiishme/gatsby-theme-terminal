6d9f077249e87c67e808d338d01813e871be09cd
ashiishme <iamashiishy@gmail.com>
2022-01-20 23:30:43 +0100

    refactor: add default text size in wrapper

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,5 +3,5 @@ import Layout from './src/components/layout'
 import './src/styles/global.css'
 
 export const wrapRootElement = ({ element }) => (
-  <Layout className={'main h-full'}> {element} </Layout>
+  <Layout className={'main h-full text-sm'}> {element} </Layout>
 )
