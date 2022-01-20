b5cee7ab78fe59cb9855a0154f6310dfe9aa7135
ashiishme <iamashiishy@gmail.com>
2022-01-20 23:16:45 +0100

    refactor: update color & margin for terminal prompt

--- src/components/terminal/terminalPrompt.tsx
+++ src/components/terminal/terminalPrompt.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import ContentEditable from '../base/contentEditable'
 
 const TerminalPrompt = () => {
   return (
     <>
-      <div className="flex flex-row terminal-prompt">
-        <p className="prompt text-sm font-normal mr-7 text-pacificBlue">
+      <div className="flex flex-row terminal-prompt mb-4">
+        <p className="prompt text-sm mr-7 text-pacificBlue">
           <span className="username">[ ashiish</span>
           <span>@</span>
           <span className="hostname">me </span>
