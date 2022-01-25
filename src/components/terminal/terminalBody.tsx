270bf3a1e1670ca80c3ee26e08125c128e702bc9
ashiishme <iamashiishy@gmail.com>
2022-01-25 20:20:05 +0100

    feat: scroll to bottom when terminal hits end

--- src/components/terminal/terminalBody.tsx
+++ src/components/terminal/terminalBody.tsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import TerminalPrompt from './terminalPrompt'
-import { HistoryProvider } from '../../context/HistoryContext'
-import TerminalOutput from '../terminal/terminalOutput'
+import TerminalLog from './terminalLog'
+import useHistory from '../../hooks/useHistory'
 
 const TerminalBody = () => {
+  const { history } = useHistory()
+
+  const handleScrollToEnd = (ref: HTMLDivElement) => {
+    if (ref) {
+      ref.scrollTop = ref.scrollHeight
+    }
+  }
+
   return (
-    <>
-      <HistoryProvider>
-        <div className="w-7/12 h-3/4 bg-mirage/[0.88] text-white rounded-lg shadow-lg p-10">
-          <TerminalPrompt />
-          <TerminalOutput />
-        </div>
-      </HistoryProvider>
-    </>
+    <div
+      ref={handleScrollToEnd}
+      className="w-7/12 h-3/4 bg-mirage/[0.88] text-white rounded-lg shadow-lg p-10 overflow-y-scroll overflow-x-hidden"
+    >
+      <TerminalLog logs={history} />
+      <TerminalPrompt />
+    </div>
   )
 }
 export default TerminalBody
