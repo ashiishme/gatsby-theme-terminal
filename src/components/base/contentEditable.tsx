b783af25d66be8f23c1605dc35776ecdea0e363b
ashiishme <iamashiishy@gmail.com>
2022-01-25 20:18:06 +0100

    refactor: remove outline from command input

--- src/components/base/contentEditable.tsx
+++ src/components/base/contentEditable.tsx
@@ -10,7 +10,7 @@ const ContentEditable = ({ className }) => {
         id="terminal-input"
         suppressContentEditableWarning
         contentEditable
-        className={`w-auto text-sm ${className}`}
+        className={`w-auto text-sm outline-none ${className}`}
         ref={elemRef}
       ></div>
     </>
