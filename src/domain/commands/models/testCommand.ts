d9cfc360ec5aee48d4a83471d2348c49a85c5e1a
ashiishme <iamashiishy@gmail.com>
2022-01-30 23:53:06 +0100

    refactor: remove test command

--- src/domain/commands/models/testCommand.ts
+++
@@ -1,9 +0,0 @@
-import { CommandInterface } from '../../../interfaces/commandInterface'
-
-export default class TestCommand implements CommandInterface {
-  constructor() {}
-
-  async execute() {
-    return 'Test Command'
-  }
-}
