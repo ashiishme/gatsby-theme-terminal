dd22eb461e277ce5498a58dd79c5ad504b168a9e
ashiishme <iamashiishy@gmail.com>
2022-01-30 23:53:39 +0100

    feat: add component mapper for cmd class

--- src/domain/commands/models/whoami.ts
+++ src/domain/commands/models/whoami.ts
@@ -1,9 +1,30 @@
 import { CommandInterface } from '../../../interfaces/commandInterface'
+import { CommandOutputProps } from '../../../types'
 
 export default class WhoAmI implements CommandInterface {
   constructor() {}
 
-  async execute(): Promise<string> {
-    return 'Hello, Ashish Yadav'
+  private toReact(): CommandOutputProps {
+    const data = {
+      component: 'TableView',
+      data: [
+        { key: 'Name', value: 'Ashish Yadav' },
+        { key: 'Location', value: 'Berlin, Germany' },
+        { key: 'Designation', value: 'Software Engineer' },
+        { key: 'Working at', value: 'Lendis' },
+        { key: 'Favorite Editor', value: 'Vs Code' },
+        { key: 'Programming Languages', value: 'JavaScript, TypeScript & C++' },
+        {
+          key: 'Talks About',
+          value: `#node, #react, #serverless, #test-driven-development, #open-source, #coding-standards, #writing-secure-code,
+        #software-engineering & #tech-community`,
+        },
+      ],
+    }
+    return data
+  }
+
+  async execute(): Promise<CommandOutputProps> {
+    return this.toReact()
   }
 }
