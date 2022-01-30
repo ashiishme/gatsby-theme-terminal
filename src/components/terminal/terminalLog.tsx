68ed1087417a523c2a42895ffe50bf900cd4af70
ashiishme <iamashiishy@gmail.com>
2022-01-30 23:50:29 +0100

    feat: render log based on command type & data

--- src/components/terminal/terminalLog.tsx
+++ src/components/terminal/terminalLog.tsx
@@ -1,22 +1,26 @@
 import React, { FC } from 'react'
 import TerminalPrompt from './terminalPrompt'
-import { HistoryProps } from '../../hooks/useHistory'
+import { HistoryProps } from '../../types'
+import TerminalMapper from '../../utils/terminalMapper'
+
 const TerminalLog: FC<{ logs: HistoryProps[] }> = ({ logs }) => {
   return (
     <>
       {logs &&
-        logs.map(({ type, data }, index: number) => (
-          <div className="flex flex-row mb-4" key={index}>
-            {type === 'command' ? (
-              <>
-                <TerminalPrompt />
-                <div>{data}</div>
-              </>
-            ) : (
-              <div>{data}</div>
-            )}
-          </div>
-        ))}
+        logs.map(({ type, output }, index) => {
+          return (
+            <div className="flex flex-row mb-4" key={index}>
+              {type === 'command' ? (
+                <>
+                  <TerminalPrompt />
+                  <div>{output}</div>
+                </>
+              ) : (
+                TerminalMapper(output)
+              )}
+            </div>
+          )
+        })}
     </>
   )
 }
